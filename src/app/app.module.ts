73adadcacc40ff3ebd8d42cd9f11174db6e003dd
Moe Alali <moe3l3ali@gmail.com>
2020-07-23 02:00:57 +0300

    graduate testimonial cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { HomeComponent } from './home/home.component';
 import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BannerComponent,
     HomeComponent,
-    TestimonialsComponent
+    TestimonialsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/testimonials/testimonials.ts
+++ src/app/components/testimonials/testimonials.ts
@@ -2,32 +2,38 @@ const TESTIMONIALS = [
     {
         name: 'John Smith',
         major: 'Space Engineer',
-        quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo in vitae turpis massa sed. Mi eget mauris pharetra et ultrices neque ornare. Integer feugiat scelerisque varius morbi enim nunc faucibus a. At erat pellentesque adipiscing commodo elit at. Tristique senectus et.'
+        quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo in vitae turpis massa sed. Mi eget mauris pharetra et ultrices neque ornare. Integer feugiat scelerisque varius morbi enim nunc faucibus a.',
+        picture: 'john-1.jpg'
     },
     {
         name: 'John Smith',
         major: 'Space Engineer',
-        quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo in vitae turpis massa sed. Mi eget mauris pharetra et ultrices neque ornare. Integer feugiat scelerisque varius morbi enim nunc faucibus a. At erat pellentesque adipiscing commodo elit at. Tristique senectus et.'
+        quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        picture: 'jane-1.jpg'
     },
     {
         name: 'John Smith',
         major: 'Space Engineer',
-        quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo in vitae turpis massa sed. Mi eget mauris pharetra et ultrices neque ornare. Integer feugiat scelerisque varius morbi enim nunc faucibus a. At erat pellentesque adipiscing commodo elit at. Tristique senectus et.'
+        quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo in vitae turpis massa sed.',
+        picture: 'john-2.jpg'
     },
     {
         name: 'John Smith',
         major: 'Space Engineer',
-        quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo in vitae turpis massa sed. Mi eget mauris pharetra et ultrices neque ornare. Integer feugiat scelerisque varius morbi enim nunc faucibus a. At erat pellentesque adipiscing commodo elit at. Tristique senectus et.'
+        quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
+        picture: 'jane-2.jpg'
     },
     {
         name: 'John Smith',
         major: 'Space Engineer',
-        quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo in vitae turpis massa sed. Mi eget mauris pharetra et ultrices neque ornare. Integer feugiat scelerisque varius morbi enim nunc faucibus a. At erat pellentesque adipiscing commodo elit at. Tristique senectus et.'
+        quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo in vitae turpis massa sed. Mi eget mauris pharetra et ultrices.',
+        picture: 'john-3.jpg'
     },
     {
         name: 'John Smith',
         major: 'Space Engineer',
-        quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo in vitae turpis massa sed. Mi eget mauris pharetra et ultrices neque ornare. Integer feugiat scelerisque varius morbi enim nunc faucibus a. At erat pellentesque adipiscing commodo elit at. Tristique senectus et.'
+        quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo in vitae turpis massa sed.',
+        picture: 'jane-3.jpg'
     }
 ];
 
