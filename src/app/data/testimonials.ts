ac2386a092d5c22bf6e08deb1c0a789b58c7c0dd
Moe Alali <moe3l3ali@gmail.com>
2020-07-27 19:53:10 +0300

    Revert "coverted all assets to webp format for quicker load times"

--- src/app/data/testimonials.ts
+++ src/app/data/testimonials.ts
@@ -3,38 +3,38 @@ const TESTIMONIALS = [
         name: 'John Smith',
         major: 'Space Engineer',
         quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo in vitae turpis massa sed. Mi eget mauris pharetra et ultrices neque ornare. Integer feugiat scelerisque varius morbi enim nunc faucibus a.',
-        picture: 'testimonials/john-1.webp'
+        picture: 'testimonials/john-1.jpg'
     },
     {
         name: 'John Smith',
         major: 'Space Engineer',
         // tslint:disable-next-line: max-line-length
         quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        picture: 'testimonials/jane-1.webp'
+        picture: 'testimonials/jane-1.jpg'
     },
     {
         name: 'John Smith',
         major: 'Space Engineer',
         quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo in vitae turpis massa sed.',
-        picture: 'testimonials/john-2.webp'
+        picture: 'testimonials/john-2.jpg'
     },
     {
         name: 'John Smith',
         major: 'Space Engineer',
         quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
-        picture: 'testimonials/jane-2.webp'
+        picture: 'testimonials/jane-2.jpg'
     },
     {
         name: 'John Smith',
         major: 'Space Engineer',
         quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo in vitae turpis massa sed. Mi eget mauris pharetra et ultrices.',
-        picture: 'testimonials/john-3.webp'
+        picture: 'testimonials/john-3.jpg'
     },
     {
         name: 'John Smith',
         major: 'Space Engineer',
         quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo in vitae turpis massa sed.',
-        picture: 'testimonials/jane-3.webp'
+        picture: 'testimonials/jane-3.jpg'
     }
 ];
 
